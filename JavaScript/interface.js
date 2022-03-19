60a192af8155c72d93ce6a81c642854a21194c43
Marlon Santos <xsantos.marlonx@gmail.com>
2022-03-19 10:37:36 -0300

    Projeto Jogo Da Velha

---
+++ JavaScript/interface.js
@@ -0,0 +1,39 @@
+document.addEventListener('DOMContentLoaded', () => {
+
+    let squares = document.querySelectorAll(".square");
+
+    squares.forEach((square) => {
+        square.addEventListener('click', handleCLick);
+    });
+});
+
+function handleCLick(event) {
+
+    let square = event.target;
+    let position = square.id;
+
+    if (handleMove(position)) {
+        setTimeout(() => {
+            alert("O jogo acabaou - O vencedor foi " + playerTime);
+        }, 10);
+
+    }
+    updateSquares();
+}
+
+function updateSquares() {
+    let squares = document.querySelectorAll(".square");
+
+    squares.forEach((square) => {
+        let position = square.id;
+        let symbol = board[position];
+
+        if (symbol != '') {
+            square.innerHTML = `<div class='${symbol}'></div>`
+        }
+    })
+}
+
+function resetGame(){
+ 
+}
